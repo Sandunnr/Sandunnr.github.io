1b144eae970f0242198be5da6b3ea989bf1ca153
sandun <sandun.snr@gmail.com>
2022-03-01 11:02:18 +0530

    Add Files

---
+++ js/templatemo-script.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function(){
+
+	// Mobile menu
+	$('.mobile-menu-icon').click(function(){
+		$('.tm-nav').toggleClass('show');
+	});
+  
+  	
+  	$('body').bind('touchstart', function() {});
+});
